fe3dae6aa6d82ea20e39babe8ae3b1ae58188cb7
greenbrian <greenbrian@gmail.com>
2017-08-14 00:03:12 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -11,12 +11,14 @@ data "google_compute_zones" "available" {}
  
 resource "google_compute_instance" "default" {
  project = "${var.project_name}"
- zone = "${data.google_compute_zones.available.names[0]}"
+ zone = "us-central1-a"
  name = "tf-compute-1"
  machine_type = "f1-micro"
- disk {
-   image = "ubuntu-1604-xenial-v20170328"
- }
+ boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8"
+    }
+  }
  network_interface {
    network = "default"
    access_config {
