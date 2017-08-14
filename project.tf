d4a1cede9028db07c99c40160386b8496249848c
greenbrian <greenbrian@gmail.com>
2017-08-13 23:41:47 -0500

    update project.tf

--- project.tf
+++ project.tf
@@ -1,6 +1,5 @@
 variable "project_name" {}
 variable "billing_account" {}
-variable "org_id" {}
 variable "region" {}
 
 provider "google" {
@@ -16,7 +15,6 @@ resource "google_project" "project" {
  name            = "${var.project_name}"
  project_id      = "${random_id.id.hex}"
  billing_account = "${var.billing_account}"
- org_id          = "${var.org_id}"
 }
 
 resource "google_project_services" "project" {
